9c1fcdc3ee14bd3003434576ad9e3184c67ffa31
Nathaniel Richards <nthnlrichards@gmail.com>
2023-03-14 11:24:08 -0400

    Connecting package

---
+++ src/engine.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Initialize } from '.';
+
+const OrionEngine = (props: { children: React.ReactNode; engine_state: React.Dispatch<React.SetStateAction<any>> }) => {
+  const [initialized, setInitialized] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!initialized) {
+      const engine = Initialize();
+      props.engine_state(engine);
+      setInitialized(true);
+    }
+  }, []);
+
+  return <>{props.children}</>;
+};
+
+export default OrionEngine;
---
+++ test.md
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+// import OrionEngine from './src/engine';
+
+// export const TestComponent = () => {
+// const [engine, setEngine] = useState<any>();
+
+// return (
+// <OrionEngine engine_state={setEngine}>
+// <p>Bruh</p>
+// </OrionEngine>
+// );
+// };
